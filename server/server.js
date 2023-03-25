93c1d1ba4ba36b94ca75196795073526133d9da4
Cody Cooper <cody_codster@live.com>
2023-03-25 11:12:04 -0500

    Installed dotenv and jsonwebtoken as dependencies. Created authorization middleware

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: AuthMiddleware
+// })
+
 //We will need to add NODE_ENV="production" in .env file when deploying.
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
@@ -15,4 +22,4 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
-}); 
+});
