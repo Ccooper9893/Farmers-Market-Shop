29a68602df8a183a37b95be51410e046476edafb
Cody Cooper <cody_codster@live.com>
2023-03-26 20:00:55 -0500

    graphql

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,30 +3,38 @@ const { Market, Product, User, Purchase } = require('../models');
 const { signToken } = require('../utils/jwt-auth');
 
 const resolvers = {
-
     Query: {
         //Get current user account and purchases
         me: async (parent, args, context) => {
-            if(context.user) {
-                return await User.findOne({email: context.user.email}).populate('purchases');
+            if (context.user) {
+                return await User.findOne({ email: context.user.email }).populate('purchases');
             };
 
             throw new AuthenticationError('No user with that email address exists!');
         },
         //Get all merchant accounts and their products
         merchants: async (parent, args) => {
-            return await User.find({merchant: true}).populate('products');
+            return await User.find({ merchant: true }).populate('products');
         },
     },
 
     Mutation: {
         //Create user and sign token
         addUser: async (parent, args) => {
+            //Save profile pic here?
             const newUser = await User.create(...args);
             const token = signToken(newUser);
-
             return { newUser, token };
+        },
+        loginUser: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) return new AuthenticationError('Incorrect credentials!');
+            //Password validation here
+
+            return user;
         },
     },
 };
+
+module.exports = resolvers;
 
