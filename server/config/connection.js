718b859aaa2b0510b512097be2bd573f138dd794
Cody Cooper <cody_codster@live.com>
2023-04-01 13:40:45 -0500

    Updated connection.js file to work with heroku and mongodb atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/farmersMarketDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/farmersmarket', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
