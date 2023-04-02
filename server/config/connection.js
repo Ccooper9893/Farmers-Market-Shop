2b9547d65772826d61c74a607610009ef4d988bd
Cody Cooper <cody_codster@live.com>
2023-04-02 18:38:45 -0500

    debugging connection.js mongodb connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/farmersmarket', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/farm-market', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
