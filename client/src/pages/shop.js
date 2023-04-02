17fbc4aa6bde626c4ee0121484347584a529e232
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-04-01 19:16:35 -0700

    debugging useState, context hook

--- client/src/pages/shop.js
+++ client/src/pages/shop.js
@@ -1,68 +1,33 @@
 import ProductCard from "../components/productCard";
 import { useQuery } from '@apollo/client';
-// Import useEffect from React.
-import React, { useState } from 'react';
-//queries
-import {GET_PRODUCTS} from '../utils/queries';
+import React from 'react';
+import { GET_PRODUCTS } from '../utils/queries';
 
 function Shop() {
   const { loading, data } = useQuery(GET_PRODUCTS);
   const products = data?.getProducts || [];
- console.log (products)
-// const [categoryData, setCategoryData] = useState(products)
-// console.log ({categoryData})
 
   return (
-    <>
-
-    <div className="bg-emerald-900 bg-opacity-20 rounded-lg shadow bg-base-200 drawer  h-screen">
-     <input id="my-drawer-2" type="checkbox" className="drawer-toggle"></input>
-       <div className="flex flex-col items-center justify-center drawer-content">
-        
-         <div className="text-xs text-center ">
-         <ProductCard 
-         // products={products}
-          products={products}
-         />
-         </div> 
-
-   </div> 
-   <div class="drawer-side p-2.5 ">
-    <label for="my-drawer-2" class="drawer-overlay"></label> 
-        {/* TO DO: map through all categories */}
- 
-      {products.map((products => (
-            <ul  className="menu p-8  overflow-y-auto w-60 h-80 rounded-lg text-slate-300 font-bold tracking-wide">
-            <li key={products._id}>
-              <p>{products.category.name}</p>
-            </li> 
-            </ul>)))} 
-
-    {/* <ul className="menu p-8  overflow-y-auto w-60 h-80 rounded-lg text-slate-300 font-bold tracking-wide">
-      <li>
-        <p>Dairy</p>
-      </li> 
-      <li>
-        <p>Meats</p>
-      </li>
-      <li>
-        <p>Category 1</p>
-      </li>
-      <li>
-        <p>Category 2</p>
-      </li>
-      <li>
-        <p>Category 3</p>
-      </li>
-    </ul> */}
-  </div>
-</div>
-
-
-
-
-
-    </>
+    
+      <div className="bg-emerald-900 bg-opacity-20 rounded-lg shadow bg-base-200 drawer h-screen">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="flex flex-col items-center justify-center drawer-content">
+          <div className="text-xs text-center">
+            <ProductCard products={products} />
+          </div>
+        </div>
+        <div class="drawer-side p-2.5">
+          <label for="my-drawer-2" class="drawer-overlay"></label>
+          {products.map((product) => (
+            <ul key={product._id} className="menu p-8 overflow-y-auto w-60 h-80 rounded-lg text-slate-300 font-bold tracking-wide">
+              <li>
+                <p>{product.category.name}</p>
+              </li>
+            </ul>
+          ))}
+        </div>
+      </div>
+    
   );
 }
 
