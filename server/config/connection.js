9d4626542ffcd205b0173fad5ce0ff6c05e4e29b
Cody Cooper <cody_codster@live.com>
2023-04-02 18:53:19 -0500

    Fixed database connection, make sure to seedgit add -A!

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/farm-market', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/farmersmarket', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
