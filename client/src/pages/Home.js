eca650423f06d01df5fd1f006c31424ab04ee53c
Cody Cooper <cody_codster@live.com>
2023-05-11 21:04:56 -0500

    Updated homepage to fix mobile responsiveness issues

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import farmerBg from "../images/f1.webp";
 function Home() {
     return (
-        <div className="relative">
+        <div>
             <h6 className="w-full pt-4 pb-2 text-center text-md bg-stone-800">Our Farmer's Market is open Monday through Friday from 11AM to 5PM</h6>
-            <div className="relative">
-            <img src={farmerBg}></img>
-            <div className="absolute bg-black w-2/5 h-3/5 bg-opacity-60 rounded-lg" style={{top: "15%", right: "10%"}}>
-                <p className="text-5xl text-center p-4 mt-10 text-white">We look forward to seeing you at the Farmer's Market!</p>
-                <p className="text-4xl text-center p-4 mt-10 text-white">Come on down and support your local farmers and artists or shop on our online store!</p>
-            </div>
+            <div className="hero min-h-screen" style={{ backgroundImage: `url(${farmerBg})` }}>
+                <div className="hero-overlay bg-opacity-30"></div>
+                <div className="hero-content text-center text-white">
+                    <div className="max-w-3xl p-10 bg-black bg-opacity-70">
+                        <h1 className="mb-5 text-2xl lg:text-4xl">We look forward to seeing you at the Farmer's Market!</h1>
+                        <p className="mb-5 text-lg lg:text-2xl">Come on down and support your local farmers and artists or shop for products on our online store!</p>
+                        <Link to="/shop" className="bg-green-900 text-white hover:bg-green-800 p-3 text-lg rounded-lg">Shop Now</Link>
+                    </div>
+                </div>
             </div>
         </div>
     )
