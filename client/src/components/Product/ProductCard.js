b57cc86d7a0925a685bb2573660b0f63850fe0cd
Cody Cooper <cody_codster@live.com>
2023-05-14 20:23:46 -0500

    Created filter and sort dropdowns for products on shop page

--- client/src/components/Product/ProductCard.js
+++ client/src/components/Product/ProductCard.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function ProductCard({ product }) {
     return (
-        <Link to="/product/:id">
+        // <Link to={`/product/${product._id}`}>
             <div className="card card-compact rounded-none hover:opacity-90">
                 <figure><img className=" w-36 h-36 lg:w-56 lg:h-56 object-cover border border-black" src={product.image} alt={product.name} /></figure>
                 <div className="text-black text-center p-2 w-36 lg:w-56 ">
@@ -13,7 +13,7 @@ function ProductCard({ product }) {
                     {/* <button className="bg-green-900 text-white hover:bg-green-800 p-2 text-md mt-2">Add to Cart</button> */}
                 </div>
             </div>
-        </Link>
+        // </Link>
     )
 };
 
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -6,21 +6,89 @@ import ProductCard from "../components/Product/ProductCard";
 function Shop() {
 
     const [products, setProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
 
     useQuery(GET_PRODUCTS, {
         onCompleted: (data) => {
-            console.log(data.getProducts);
+            setFilteredProducts(data.getProducts);
             setProducts(data.getProducts);
         },
     });
 
+    const filterByCategory = (event) => {
+        const selection = event.target.id;
+        if (selection === "All") {
+            setFilteredProducts(products);
+        } else {
+            const updatedProducts = products.filter(
+                (product) => product.category === selection
+            );
+            setFilteredProducts(updatedProducts);
+        }
+    };
+
+    const sortProducts = (event) => {
+        const selection = event.target.id;
+        switch (selection) {
+            case "AZ": {
+                const tempFilter = [...filteredProducts].sort((a, b) => a.name.localeCompare(b.name));
+                setFilteredProducts(tempFilter);
+            }
+                break;
+                case "ZA": {
+                    const tempFilter = [...filteredProducts].sort((a, b) => b.name.localeCompare(a.name));
+                    setFilteredProducts(tempFilter);
+                }
+                break;
+                case "HL": {
+                    const tempFilter = [...filteredProducts].sort((a, b) => b.price - a.price);
+                    setFilteredProducts(tempFilter);
+                }
+                break;
+                case "LH": {
+                    const tempFilter = [...filteredProducts].sort((a, b) => a.price - b.price);
+                    setFilteredProducts(tempFilter);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
-        <div className="flex justify-around flex-wrap p-4 lg:p-10 gap-2 lg:gap-8 lg:mx-44">
-            {products.map((product) => {
-                return (
-                    <ProductCard product={product} key={product._id}/>
-                )
-            })}
+        <div className="mx-4 lg:mx-96 mt-8">
+            <p className="text-black text-center text-lg lg:text-xl border-b border-black pb-4">Our farm-fresh produce is grown without the use of harmful pesticides or chemicals, so you can enjoy the freshest and healthiest fruits and vegetables possible. Plus, by supporting our local farmers, you're helping to create a more sustainable food system and a stronger community.</p>
+            <div className="flex justify-center gap-10 mt-6">
+                <div className="dropdown dropdown-center">
+                    <label tabIndex={0} className="bg-green-900 text-white hover:bg-green-800 p-2 text-xl">Filter</label>
+                    <ul tabIndex={0} className="dropdown-content menu p-2 shadow text-black bg-white w-52">
+                        <li><button id="All" onClick={filterByCategory}>All</button></li>
+                        <li><button id="Meat" onClick={filterByCategory}>Meat</button></li>
+                        <li><button id="Dairy" onClick={filterByCategory}>Dairy</button></li>
+                        <li><button id="Livestock" onClick={filterByCategory}>Livestock</button></li>
+                        <li><button id="Fruit" onClick={filterByCategory}>Fruit</button></li>
+                        <li><button id="Vegetable" onClick={filterByCategory}>Vegetable</button></li>
+                        <li><button id="Bread" onClick={filterByCategory}>Bread</button></li>
+                        <li><button id="Art" onClick={filterByCategory}>Art</button></li>
+                    </ul>
+                </div>
+                <div className="dropdown dropdown-center">
+                    <label tabIndex={0} className="bg-green-900 text-white hover:bg-green-800 p-2 text-xl">Sort by</label>
+                    <ul tabIndex={0} className="dropdown-content menu p-2 shadow text-black bg-white w-52">
+                        <li><button id="AZ" onClick={sortProducts}>Alphabetically, A-Z</button></li>
+                        <li><button id="ZA" onClick={sortProducts}>Alphabetically, Z-A</button></li>
+                        <li><button id="LH" onClick={sortProducts}>Price, low to high</button></li>
+                        <li><button id="HL" onClick={sortProducts}>Price, high to low</button></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="flex justify-around flex-wrap lg:p-10 gap-2 lg:gap-8">
+                {filteredProducts.map((product) => {
+                    return (
+                        <ProductCard product={product} key={product._id} />
+                    )
+                })}
+            </div>
         </div>
     )
 };
