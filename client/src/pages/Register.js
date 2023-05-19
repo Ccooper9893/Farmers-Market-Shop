601bece5659dc70ff5f19a82b31871256c3ba296
Cody Cooper <cody_codster@live.com>
2023-05-18 22:52:11 -0500

    Updated registration for to include image upload capabilities for merchants

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -27,10 +27,10 @@ function Register() {
 
     useEffect(() => {
         setFormState((prevState) => ({
-          ...prevState,
-          merchant: checked,
+            ...prevState,
+            merchant: checked,
         }));
-        if(!checked) {
+        if (!checked) {
             setFormState((prevState) => ({
                 ...prevState,
                 businessName: "",
@@ -40,9 +40,9 @@ function Register() {
                 address: "",
             }))
         }
-      }, [checked]);
+    }, [checked]);
 
-      const handleFormChange = (event) => {
+    const handleFormChange = (event) => {
         const { name, value } = event.target;
         setFormState({
             ...formState,
@@ -55,11 +55,18 @@ function Register() {
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         console.log(formState);
+
+        //Grabbing imageUrl and appending to productData if valid
+        const data = await uploadImage(event);
+        if (data) {
+            formState.image = data.imageUrl;
+        };
+
         try {
             const { data } = await addUser({
                 variables: { ...formState },
             });
-            
+
             Auth.login(data.addUser.token);
             setFormState({
                 email: '',
@@ -87,8 +94,25 @@ function Register() {
     );
     */
 
+    async function uploadImage(event) {
+        const formData = new FormData();
+        formData.append("image", event.target.image.files[0]);
+        if (formData) {
+            try {
+                const response = await fetch("/upload", {
+                    method: "POST",
+                    body: formData,
+                });
+                return await response.json();
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
     return (
-        <div className="flex flex-col items-center mt-24 h-full">
+        <div className="flex flex-col items-center mt-10 h-full">
             <form className="flex flex-col items-center gap-4 p-6 w-96 secFont" onSubmit={handleFormSubmit}>
                 <h1 className="text-3xl border-b border-green-800 w-full text-center">Register</h1>
                 <div className="form-control w-full max-w-xs">
@@ -132,62 +156,68 @@ function Register() {
                 </div>
                 {checked && <>
                     <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text text-black">BUSINESS NAME</span>
-                    </label>
-                    <input
-                        className="input border border-black w-full max-w-xs rounded-none"
-                        type="text"
-                        name="businessName"
-                        placeholder="name of your business"
-                        value={formState.business_name}
-                        onChange={handleFormChange}
-                    />
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text text-black">BUSINESS DESCRIPTION</span>
-                    </label>
-                    <input
-                        className="input border border-black w-full max-w-xs rounded-none"
-                        type="text"
-                        name="businessDescription"
-                        placeholder="provide a description"
-                        value={formState.business_description}
-                        onChange={handleFormChange}
-                    />
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text text-black">PHONE NUMBER</span>
-                    </label>
-                    <input
-                        className="input border border-black w-full max-w-xs rounded-none"
-                        type="tel"
-                        name="phoneNumber"
-                        placeholder="xxx-xxx-xxxx"
-                        value={formState.phone_number}
-                        onChange={handleFormChange}
-                    />
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text text-black">ADDRESS</span>
-                    </label>
-                    <input
-                        className="input border border-black w-full max-w-xs rounded-none"
-                        type="text"
-                        name="address"
-                        placeholder="address"
-                        value={formState.address}
-                        onChange={handleFormChange}
-                    />
-                </div>
-                    </>}
+                        <label className="label">
+                            <span className="label-text text-black">BUSINESS NAME</span>
+                        </label>
+                        <input
+                            className="input border border-black w-full max-w-xs rounded-none"
+                            type="text"
+                            name="businessName"
+                            placeholder="name of your business"
+                            value={formState.businessName}
+                            onChange={handleFormChange}
+                        />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text text-black">UPLOAD PICTURE</span>
+                        </label>
+                        <input type="file" name="image" className="file-input border border-black rounded-none w-full"/>
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text text-black">BUSINESS DESCRIPTION</span>
+                        </label>
+                        <input
+                            className="input border border-black w-full max-w-xs rounded-none"
+                            type="text"
+                            name="businessDescription"
+                            placeholder="provide a description"
+                            value={formState.businessDescription}
+                            onChange={handleFormChange}
+                        />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text text-black">PHONE NUMBER</span>
+                        </label>
+                        <input
+                            className="input border border-black w-full max-w-xs rounded-none"
+                            type="tel"
+                            name="phoneNumber"
+                            placeholder="xxx-xxx-xxxx"
+                            value={formState.phoneNumber}
+                            onChange={handleFormChange}
+                        />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text text-black">ADDRESS</span>
+                        </label>
+                        <input
+                            className="input border border-black w-full max-w-xs rounded-none"
+                            type="text"
+                            name="address"
+                            placeholder="address"
+                            value={formState.address}
+                            onChange={handleFormChange}
+                        />
+                    </div>
+                </>}
                 <div className="form-control">
                     <label className="label cursor-pointer">
                         <span className="label-text mx-4 secFont">Are you a merchant?</span>
-                        <input type="checkbox" className="checkbox checkbox-secondary" onClick={handleCheck}/>
+                        <input type="checkbox" className="checkbox checkbox-secondary" onClick={handleCheck} />
                     </label>
                 </div>
                 <button className="bg-green-900 text-white hover:bg-green-800 px-3 py-2 w-32" type="submit">Sign Up</button>
